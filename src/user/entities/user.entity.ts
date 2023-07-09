d1826e73183b3d01ccda977c389e0d018907542b
Bruno Silva <86797184+brumsilva@users.noreply.github.com>
2023-07-09 14:01:46 -0300

    fix: adjust entities and schemas.

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,6 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Movie } from "./movie.entity";
+
 export class User {
   id?: number;
   email: string;
   password: string;
   name: string;
+  favoriteMovies: Movie[];
 }
--- src/user/schemas/film.schema.ts
+++ src/user/schemas/film.schema.ts
@@ -1,6 +1,18 @@
 /* eslint-disable prettier/prettier */
 import * as mongoose from 'mongoose';
 export const FilmManagement = new mongoose.Schema({
-  name: String,
-  liked: Boolean,
+  adult: Boolean,
+  backdrop_path: String,
+  genre_ids: Array<number>,
+  id: Number,
+  original_language: String,
+  original_title: String,
+  overview: String,
+  popularity: Number,
+  poster_path: String,
+  release_date: String,
+  title: String,
+  video: Boolean,
+  vote_average: Number,
+  vote_count: Number,
 });
